e854af857a3715eb26de0dedc9f064188fa8f824
Josue Silva Santos Junior <josue.junio@grupomateus.com>
2025-09-01 01:12:42 -0300

    first commit

---
+++ src/main/java/com/payments/hreureka/HrEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.payments.hreureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrEurekaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/payments/hreureka/HrEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payments.hreureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
